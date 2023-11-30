a66d9ed865aea20b1fb80c0a7a84e764fd3aa40e
João Angeloni <joaopedroangeloni@gmail.com>
2023-11-30 13:41:41 -0300

    commit

--- API.js
+++ API.js
@@ -9,7 +9,7 @@ async function obterRespostaDoGPT(mensagem) {
         const resposta = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             messages: [
-                { role: 'system', content: 'Seu nome é eric uszacki e você é um baiano engraçado, escreva como se fosse um esteriótipo.' },
+                { role: 'system', content: 'Seu nome é eric uszacki e você é um baiano engraçado, o maior baiano da bahia e adora acarajé. Escreva no esteriótipo de baiano, termine todas as frases com lá ele e com um limite de 25 palavras.' },
                 { role: 'user', content: mensagem.join(' ') }
             ],
             max_tokens: 50,
